25679469fabf65702a11f091e11f0d1780855426
segalophir <segalophir@gmail.com>
2024-12-09 16:34:38 +0200

    first

--- app/src/main/java/com/example/exxo/MainActivity2.kt
+++ app/src/main/java/com/example/exxo/MainActivity2.kt
@@ -1,28 +1,20 @@
 package com.example.exxo
 
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val playagainButton: Button = findViewById(R.id.main_activity_play_again_button)
-        playagainButton.setOnClickListener{
-
-        }
-
     }
 }
